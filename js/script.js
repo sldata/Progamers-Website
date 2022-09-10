079205d762a6cc611a67be9d0323cad51242f68e
Slawek01~ <lilcrunker@gmail.com>
2022-09-10 23:29:58 +0200

    rebuild website, created Portfolio section and create carousel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
+// adding listener when website is loaded
 document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
+	const nav = document.querySelector('.navbar')
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
+	// adding background shadow on navigation when scroll is greater or equal 200px on scroll
+	function addShadow() {
+		if (window.scrollY >= 200) {
+			nav.classList.add('shadow-bg')
+		} else {
+			nav.classList.remove('shadow-bg')
+		}
+	}
+	// removing navigation after clicking
+	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
 
-    function addShadow() {
-        if (window.scrollY >= 300) {
-            nav.classList.add('shadow-bg')
-        } else {
-            nav.classList.remove('shadow-bg')
-        }
-    }
-
-    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-
-    window.addEventListener('scroll', addShadow)
+	window.addEventListener('scroll', addShadow)
 })
