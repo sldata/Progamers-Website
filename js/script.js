fd44cc5e7584c5977c445c961371c3d7420074ba
Slawek01~ <lilcrunker@gmail.com>
2022-09-11 21:04:55 +0200

    repair scrollspy

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // adding listener when website is loaded
 document.addEventListener('DOMContentLoaded', function () {
+	const link = document.querySelectorAll('a')
 	const nav = document.querySelector('.navbar')
 	const allNavItems = document.querySelectorAll('.nav-link')
 	const navList = document.querySelector('.navbar-collapse')
+	const aboutUs = document.querySelector('.aboutus')
 	// adding background shadow on navigation when scroll is greater or equal 200px on scroll
 	function addShadow() {
 		if (window.scrollY >= 200) {
@@ -11,8 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
 			nav.classList.remove('shadow-bg')
 		}
 	}
+
+
+	
+
 	// removing navigation after clicking
 	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
 
 	window.addEventListener('scroll', addShadow)
 })
+
